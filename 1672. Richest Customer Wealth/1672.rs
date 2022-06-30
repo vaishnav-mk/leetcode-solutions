8b8c1ada6d12a6deafac420287aac29636c6290f
vaishnav-mk <84540554+vaishnav-mk@users.noreply.github.com>
2022-06-30 23:21:19 +0300

    Create 1672.rs

---
+++ 1672. Richest Customer Wealth/1672.rs
@@ -0,0 +1,9 @@
+impl Solution {
+    pub fn maximum_wealth(accounts: Vec<Vec<i32>>) -> i32 {
+        let mut max: i32 = 0;
+        for customer in accounts.iter()
+        {
+            let wealth:
+        }
+    }
+}
