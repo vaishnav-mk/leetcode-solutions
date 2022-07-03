f4c5b6eb6bcd4c50265cf2ccbd0b5bff01e4cc68
vaishnav-mk <84540554+vaishnav-mk@users.noreply.github.com>
2022-07-03 20:39:51 +0300

    Create 344.rs
    Runtime: 25 ms, faster than 31.25% of Rust online submissions for Reverse String.
    Memory Usage: 5.4 MB, less than 99.43% of Rust online submissions for Reverse String.

---
+++ 344. Reverse String/344.rs
@@ -0,0 +1,10 @@
+impl Solution {
+    pub fn reverse_string(s: &mut Vec<char>) {
+        s.reverse()
+    }
+}
+      
+/*
+Runtime: 25 ms, faster than 31.25% of Rust online submissions for Reverse String.
+Memory Usage: 5.4 MB, less than 99.43% of Rust online submissions for Reverse String.
+*/
