5474e92f5fe0d975c7732b061ef01e245a67b9c2
Vaishnav <84540554+vaishnav-mk@users.noreply.github.com>
2022-10-06 07:01:47 +0530

    Runtime1 ms Beats 90.44% Memory39.8 MB Beats 92.21%

---
+++ 410. Split Array Largest Sum/410.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int splitArray(int[] nums, int m) {
+        int start = 0;
+        int end = 0;
+
+        for (int i = 0; i<nums.length; i++) {
+            start = Math.max(start, nums[i]);
+            end += nums[i];
+        }
+
+        while (start < end) {
+            int mid = start + (end-start)/2;
+            int sum = 0;
+            int pieces = 1;
+            for (int num: nums) {
+                if (sum + num > mid) {
+                    sum = num;
+                    pieces++;
+                } else {
+                    sum += num;
+                }
+            }
+
+            if (pieces > m) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        return end;
+    }
+}
+
+/*
+Runtime1 ms
+Beats
+90.44%
+Memory39.8 MB
+Beats
+92.21%
+*/
