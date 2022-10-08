ee40772178bb60a3c16ba03222038e97011afca0
Vaishnav <84540554+vaishnav-mk@users.noreply.github.com>
2022-10-08 06:09:04 +0530

    Runtime17 ms Beats 42.21% Memory70.1 MB Beats 17.30%

---
+++ 442. Find All Duplicates in an Array/442.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+            if (arr[i] != arr[correct]) {
+                swap(arr, i, correct);
+            } else {
+                i++;
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index] != index+1) {
+                ans.add(arr[index]);
+            }
+        }
+        return ans;
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
+
+/*
+Runtime17 ms
+Beats
+42.21%
+Memory70.1 MB
+Beats
+17.30%
+*/
