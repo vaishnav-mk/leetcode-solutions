8b13922d8a6e4441017c5b93a3480eaad97eef6e
Vaishnav <84540554+vaishnav-mk@users.noreply.github.com>
2022-10-08 05:39:22 +0530

    Runtime11 ms Beats 53.33% Memory76 MB Beats 37.83%

---
+++ 287. Find the Duplicate Number/287.java
@@ -0,0 +1,34 @@
+class Solution {
+  public int findDuplicate(int[] arr) {
+    int i = 0;
+    while (i < arr.length) {
+      if (arr[i] != i + 1) {
+        int correct = arr[i] - 1;
+        if (arr[i] != arr[correct]) {
+          swap(arr, i, correct);
+        } else {
+          return arr[i];
+        }
+      } else {
+        i++;
+      }
+    }
+    return -1;
+
+  }
+
+  static void swap(int[] arr, int first, int second) {
+    int temp = arr[first];
+    arr[first] = arr[second];
+    arr[second] = temp;
+  }
+}
+
+/*
+Runtime11 ms
+Beats
+53.33%
+Memory76 MB
+Beats
+37.83%
+*/
