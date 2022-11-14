49ade53efa0ee47ca9af6049602ec94bcc90925e
vaishnav <84540554+vaishnav-mk@users.noreply.github.com>
2022-11-14 11:15:37 +0530

    Runtime 76 ms Beats 90.32% Memory 44.1 MB Beats 77.38%

---
+++ 237. Delete Node in a Linked List/237.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function(node) {
+    node.val = node.next.val
+    node.next = node.next.next
+};
+
+/*
+Runtime
+76 ms
+Beats
+90.32%
+Memory
+44.1 MB
+Beats
+77.38%
+*/
